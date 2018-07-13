52a586b4f4f6cd1893ec66b4cfa1bdb8bff9cd27
wjp <wjp@frontlineprocessing.com>
2018-07-13 16:24:48 -0600

    Working out distance test

--- test/vault.test.js
+++ test/vault.test.js
@@ -1,20 +1,21 @@
 const expect = require('chai').expect;
+const { degrees, radians } = require('radians')
 
 describe('Vault Tests', () => {
     describe('Reverse Array', () => {
         it('should turn the below string into an array and reverse the words', () => {
             let data = "I want this job.";
 
-            // Code here
-
+            data = data.replace(".","").split(" ");
+            data = data.reverse();
             expect(['job', 'this', 'want', 'I']).to.deep.equal(data);
         });
     });
     describe('Order Array', () => {
         it('should sort the below array', () => {
             let data = ['200', '450', '2.5', '1', '505.5', '2'];
-
-            // Code here
+            data = data.map(Number);
+            data = data.sort()
 
             expect([1, 2, 2.5, 200, 450, 505.5]).to.deep.equal(data);
         });
@@ -24,11 +25,25 @@ describe('Vault Tests', () => {
             let data1 = [1, 2, 3, 4, 5, 6, 7];
             let data2 = [2, 4, 5, 7, 8, 9, 10];
 
-            // Code here
+            let data = []
+            data2.forEach(function(item){
+              let i = data1.indexOf(item)
+              if(i != -1){
+              }else{
+                data.push(item)
+              }
+            });
 
             expect([8, 9, 10]).to.deep.equal(data);
 
-            // Code here
+            data = []
+            data1.forEach(function(item){
+              let i = data2.indexOf(item)
+              if(i != -1){
+              }else{
+                data.push(item)
+              }
+            });
 
             expect([1, 3, 6]).to.deep.equal(data);
         });
@@ -43,9 +58,18 @@ describe('Vault Tests', () => {
                 lat: '42.1820210',
                 lon: '-88.3429465',
             };
+            const R = 6371e3
 
-            // Code here
+            let lat1 = place1["lat"]
+            let lat2 = place2["lat"]
+            let lat = place2["lat"] - place1["lat"]
+            let lon = place2["lon"] - place1["lon"]
+            console.log(lat, lon);
 
+            var a = Math.sin(lat/2) * Math.sin(lat/2) + Math.cos(lat1) * Math.cos(lat2) * Math.sin(lon/2) * Math.sin(lon/2);
+            var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+            let distance = R * c;
+            console.log(distance);
             expect(distance).to.equal('36.91');
         });
     });
